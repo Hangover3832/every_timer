d651f2d2ed47899b4c57c803fc57a7ad491832df
AlexL <github@leual.ch>
2025-12-27 10:51:17 +0100

    update demo code

--- every_demo.py
+++ every_demo.py
@@ -34,22 +34,34 @@ def Demo():
 
     counter = 0
 
-    @Every.While(30) # repeat for 30s
-    def timed_while():
+    @Every.While(10, param="Timed while loop decorator usage") # repeat for 10s
+    def execute_demo(param:str):
         nonlocal counter
 
         VerySimple()
 
-        executed, res = MyFunction1(param1=counter) # Add param1 dynamically
-        if executed:
-            print(f"Function1 returned: {res}, function2 time remaining: {my_function2_timer.time_remaining:.2f}s")
+        if MyFunction1(param1=counter): # Add param1 dynamically
+            print(f"Function1 returned: {MyFunction1.result}, function2 time remaining: {my_function2_timer.time_remaining:.2f}s")
             counter += 1
+            print(f"{MyFunction1.kwargs=}")
 
-        executed, res = my_function2_timer(b=2) # Override b dynamically
-        if executed:
-            print(f"Function2 returned: {res}, function1 time remaining: {MyFunction1.time_remaining:.2f}s")
+        if my_function2_timer(b=2): # Override b dynamically
+            print(f"Function2 returned: {my_function2_timer.result}, function1 time remaining: {MyFunction1.time_remaining:.2f}s")
+            print(f"{my_function2_timer.kwargs=}")
 
         sleep(0.01)
+        return param
+
+    print(f"Loop exited with: '{execute_demo.result}'")
+
+    # or:
+    # Every(10).do(execute_demo).do_while(param="Not decorated")
+
+    # or:
+    # timed_while = Every(10).do(execute_demo).among(param="Not decorated")
+    # timed_while.do_while()
+    # ...
+    # result = timed_while.do_while().result
 
 
 if __name__ == "__main__":
